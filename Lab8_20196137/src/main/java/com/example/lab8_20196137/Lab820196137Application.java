99493aff60874d78eac44b995f17899b5265e4ee
Jhondpc <a20196137@pucp.edu.pe>
2023-06-13 20:35:44 -0500

    proyecto creado

---
+++ Lab8_20196137/src/main/java/com/example/lab8_20196137/Lab820196137Application.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20196137;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab820196137Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab820196137Application.class, args);
+    }
+
+}
